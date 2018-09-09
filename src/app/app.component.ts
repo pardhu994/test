39d67529a1c42ca40de53956e9e2377677e8f14b
jatin <jkhurana09988@gmail.com>
2018-09-09 19:42:41 +0530

    added files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-
+import {HttpClient}  from '@angular/common/http';
 
 import { FormGroup, FormControl,NgForm ,FormBuilder} from '@angular/forms';
 
@@ -9,37 +9,102 @@ import { FormGroup, FormControl,NgForm ,FormBuilder} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
- 
-  table1:boolean=true;
-  table2:boolean=true;
-  table3:boolean=true;
-  table4:boolean=true;
-  table5:boolean=true;
-  id:any;
+ response:any;
+ dataSourceC1:Array<any>=[];
+ dataSourceC2:Array<any>=[];
+ dataSourceC3:Array<any>=[];
+ dataSourceC4:Array<any>=[];
+ dataSource:any;
+ dataSourcec1:any;
+ dataSourcec2:any;
+ dataSourcec3:any;
+ dataSourcec4:any;
+constructor(private http:HttpClient){}
   ngOnInit(){
+    this.http.get('https://demo8472939.mockable.io/jsonDataoutput').subscribe(data=>this.statusData(data));
+  
+  this.dataSourcec1 = {
+    "data": this.dataSourceC1
+} 
+this.dataSourcec2 = {
+    "data": this.dataSourceC2
+} 
 
+this.dataSourcec3 = {
+    "data": this.dataSourceC3
+} 
+
+this.dataSourcec4 = {
+    "data": this.dataSourceC4
+} 
   }
-  deleteTable(){
-    console.log(this.id);
-    switch(this.id) {
-      case '1':
-          this.table1=false;
-          break;
-      case '2':
-          this.table2=false;
-          break;
-      case '3':
-          this.table3=false;
-          break;
-      case '4':
-          this.table4=false;
-          break;
-      case '5':
-          this.table5=false;
-          break;
-     
-  } 
-  }
+
+  statusData(data){
+      this.response=data;
+      console.log(this.response.component);
+
+for(var i=0;i<this.response.component.length;i++){
+    if(i==0)
+    {
+        let status1:any=0;
+        let status2:any=0;
+        for(var j=0;j<this.response.component[i].probes.length;j++){
+            if(this.response.component[i].probes[j].status===1){
+               status1++;
+            }
+            else{
+                status2++;
+            }
+        }
+        this.dataSourceC1.push({'label':'Status1',value:status1,"color":"#008000"},{'label':'Status2',value:status2,"color":"000000"});
+    }
+
+    if(i==1){
+
+        let status1:any=0;
+        let status2:any=0;
+        for(var j=0;j<this.response.component[i].probes.length;j++){
+            if(this.response.component[i].probes[j].status===1){
+               status1++;
+            }
+            else{
+                status2++;
+            }
+        }
+        this.dataSourceC2.push({'label':'Status1',value:status1,"color":"#008000"},{'label':'Status2',value:status2,"color":'000000'});
+    
+    }
+    if(i==2){
+
+        let status1:any=0;
+        let status2:any=0;
+        for(var j=0;j<this.response.component[i].probes.length;j++){
+            if(this.response.component[i].probes[j].status===1){
+               status1++;
+            }
+            else{
+                status2++;
+            }
+        }
+        this.dataSourceC3.push({'label':'Status1',value:status1,"color":"#008000"},{'label':'Status2',value:status2,"color":"#000000"});
+        
+    }
+
+    if(i==3){
+
+        let status1:any=0;
+        let status2:any=0;
+        for(var j=0;j<this.response.component[i].probes.length;j++){
+            if(this.response.component[i].probes[j].status===1){
+               status1++;
+            }
+            else{
+                status2++;
+            }
+        }
+        this.dataSourceC4.push({'label':'Status1',value:status1,"color":"#008000"},{'label':'Status2',value:status2,"color":"#000000"});
+    }
 }
+  }}
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppComponent } from './app.component';
 import { FusionChartsModule } from 'angular-fusioncharts';
 import { NgForm } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 // Import FusionCharts library and chart modules
 import FusionCharts from 'fusioncharts/core';
 import Column2d from 'fusioncharts/viz/column2d';
-import FusionTheme from 'fusioncharts/themes/es/fusioncharts.theme.fusion';
-import PowerCharts from 'fusioncharts/powercharts';
+//import FusionTheme from 'fusioncharts/themes/es/fusioncharts.theme.fusion';
+//import PowerCharts from 'fusioncharts/powercharts';
 // Pass the fusioncharts library and chart modules
-FusionChartsModule.fcRoot(FusionCharts, Column2d, FusionTheme,PowerCharts);
+FusionChartsModule.fcRoot(FusionCharts, Column2d);
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ FusionChartsModule.fcRoot(FusionCharts, Column2d, FusionTheme,PowerCharts);
     BrowserModule,
     ReactiveFormsModule,
     FormsModule,
-    FusionChartsModule
+    FusionChartsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
